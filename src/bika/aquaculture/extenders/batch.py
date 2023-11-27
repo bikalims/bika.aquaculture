e5ffa812a2dd513e0f4bbe5a0b9c699ca06767ff
Lunga Baliwe <lunga001@gmail.com>
2023-11-27 19:19:38 +0200

    Changed Client Batch ID to Client Case ID

---
+++ src/bika/aquaculture/extenders/batch.py
@@ -0,0 +1,21 @@
+from archetypes.schemaextender.interfaces import ISchemaModifier
+from bika.lims.interfaces import IBatch
+from bika.aquaculture.config import is_installed
+from zope.component import adapts
+from zope.interface import implements
+
+
+class BatchSchemaModifier(object):
+    adapts(IBatch)
+    implements(ISchemaModifier)
+
+    def __init__(self, context):
+        self.context = context
+
+    def fiddle(self, schema):
+        """
+        """
+        if is_installed():
+            schema['ClientBatchID'].widget.label = "Client Case ID"
+
+        return schema
