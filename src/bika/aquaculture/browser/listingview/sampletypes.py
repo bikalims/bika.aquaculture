d0d8bc698d7d059a4ba26496b29ae1ea598fa3ae
Lunga Baliwe <lunga001@gmail.com>
2023-11-28 11:59:49 +0200

    SampleTypes listing to Specimen Types listing

---
+++ src/bika/aquaculture/browser/listingview/sampletypes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from zope.component import adapts
+from zope.interface import implements
+
+from bika.aquaculture.config import _
+from bika.aquaculture.config import is_installed
+from senaite.app.listing.interfaces import IListingView
+from senaite.app.listing.interfaces import IListingViewAdapter
+
+
+class SampleTypesListingViewAdapter(object):
+    adapts(IListingView)
+    implements(IListingViewAdapter)
+
+    def __init__(self, listing, context):
+        self.listing = listing
+        self.context = context
+
+    def before_render(self):
+        if not is_installed():
+            return
+        self.listing.title = self.context.translate(_("Specimen Types"))
+        if "Title" in self.listing.columns:
+            self.listing.columns['Title']['title'] = "Specimen Type"
+
+    def folder_item(self, obj, item, index):
+        if not is_installed():
+            return item
+        return item
