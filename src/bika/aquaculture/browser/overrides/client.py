9c4cc2287ea7f373d66c8ce3571455b5993f1305
Lunga Baliwe <lunga001@gmail.com>
2023-11-29 07:36:18 +0200

    Client Sample Points Listing override

---
+++ src/bika/aquaculture/browser/overrides/client.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.CORE.
+#
+# SENAITE.CORE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2021 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from bika.aquaculture.config import _
+from bika.aquaculture.config import is_installed
+from bika.lims.controlpanel.bika_samplepoints import SamplePointsView
+
+
+class ClientSamplePointsView(SamplePointsView):
+    """This is displayed in the "Sample Points" tab on each client
+    """
+
+    def before_render(self):
+        if not is_installed():
+            return
+        self.title = self.context.translate(_("Ponds"))
+        if "Title" in self.columns:
+            self.columns['Title']['title'] = "Pond"
+        if "SampleTypes" in self.columns:
+            self.columns['SampleTypes']['title'] = "Specimen Types"
+
+    def folder_item(self, obj, item, index):
+        if not is_installed():
+            return item
+        return item
