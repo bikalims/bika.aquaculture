0b8c456c63d615436f6321f97127f10497c5b604
Lunga Baliwe <lunga001@gmail.com>
2023-12-03 21:26:27 +0200

    Added batch fields NAN and Reference Number

--- src/bika/aquaculture/extenders/batch.py
+++ src/bika/aquaculture/extenders/batch.py
@@ -1,8 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
 from archetypes.schemaextender.interfaces import ISchemaModifier
-from bika.lims.interfaces import IBatch
-from bika.aquaculture.config import is_installed
-from zope.component import adapts
+from archetypes.schemaextender.interfaces import ISchemaExtender
 from zope.interface import implements
+from zope.component import adapts
+from zope.interface import implementer
+from Products.Archetypes.Widget import StringWidget
+
+from bika.aquaculture.config import _
+from bika.aquaculture.config import is_installed
+from bika.aquaculture.interfaces import IBikaAquacultureLayer
+from bika.extras.extenders.fields import ExtStringField
+from bika.lims.interfaces import IBatch
+
+
+nan_field = ExtStringField(
+    "NAN",
+    mode="rw",
+    schemata="default",
+    widget=StringWidget(
+        label=_(u"NAN"),
+        description=_(u"NAN"),
+    ),
+)
+
+reference_number_field = ExtStringField(
+    "ReferenceNumber",
+    mode="rw",
+    schemata="default",
+    widget=StringWidget(
+        label=_(u"Reference Number"),
+        description=_(u"Reference Number"),
+    ),
+)
+
+
+@implementer(ISchemaExtender, IBrowserLayerAwareExtender)
+class BatchSchemaExtender(object):
+    adapts(IBatch)
+    layer = IBikaAquacultureLayer
+
+    fields = [
+        nan_field,
+        reference_number_field,
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+
+    def getFields(self):
+        return self.fields
 
 
 class BatchSchemaModifier(object):
