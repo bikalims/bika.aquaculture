e2611ec37dac68f98a2694f4edfd23856ddba47c
Lunga Baliwe <lunga001@gmail.com>
2023-12-05 16:30:04 +0200

    Purpose of Testing content type

--- src/bika/aquaculture/setuphandlers.py
+++ src/bika/aquaculture/setuphandlers.py
@@ -2,9 +2,11 @@
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
-from bika.lims import api
+from bika.aquaculture.config import _
 from bika.aquaculture.config import PROFILE_ID
 from bika.aquaculture.config import logger
+from bika.lims import api
+from senaite.core.setuphandlers import add_dexterity_items
 
 
 @implementer(INonInstallable)
@@ -25,6 +27,7 @@ def post_install(context):
     context = context._getImportContext(profile_id)
     portal = context.getSite()
     setup(portal)
+    add_dexterity_setup_items(portal)
 
 
 def uninstall(context):
@@ -32,17 +35,33 @@ def uninstall(context):
     # Do something at the end of the uninstallation of this package.
 
 
+def add_dexterity_setup_items(portal):
+    """Adds the Dexterity Container in the Setup Folder
+
+    N.B.: We do this in code, because adding this as Generic Setup Profile in
+          `profiles/default/structure` flushes the contents on every import.
+    """
+    # Tuples of ID, Title, FTI
+    items = [
+        ("purpose_of_testing_folder",
+         "Purpose of Testing Folder",
+         "PurposeOfTestingFolder")
+    ]
+    setup = api.get_setup()
+    add_dexterity_items(setup, items)
+
+
 def setup(portal):
     # Batches
     batches = portal['batches']
-    batches.title = 'Cases'
+    batches.title = _('Cases')
     batches.reindexObject()
 
     # pt
     pt = api.get_tool("portal_types", context=portal)
     # Batch
     fti = pt.get("Batch")
-    fti.title = 'Case'
+    fti.title = _('Case')
 
     # Methods
     methods = portal['methods']
@@ -50,27 +69,27 @@ def setup(portal):
     methods.reindexObject()
     # Method
     fti = pt.get("Method")
-    fti.title = 'Protocol'
+    fti.title = _('Protocol')
 
     # Client
     fti = pt.get("Client")
     actions = fti.listActions()
     for idx, action in enumerate(actions):
         if action.title == "Batches":
-            action.title = "Cases"
+            action.title = _("Cases")
         if action.title == "Sample Points":
-            action.title = "Ponds"
+            action.title = _("Ponds")
 
     # Sample Types
     portal['bika_setup']["bika_sampletypes"].setTitle("Specimen Types")
     # Sample Type
     fti = pt.get("SampleType")
-    fti.title = 'Specimen Type'
+    fti.title = _('Specimen Type')
 
     # Sample Points
     portal['bika_setup']["bika_samplepoints"].setTitle("Ponds")
     # Sample Type
     fti = pt.get("SamplePoint")
-    fti.title = 'Pond'
+    fti.title = _('Pond')
 
     logger.info("BIKA.AQUACULTURE setup [DONE]")
