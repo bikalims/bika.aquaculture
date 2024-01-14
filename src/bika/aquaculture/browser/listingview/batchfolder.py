f18353302ba058541bc03033096589f9aec4edb0
Lunga Baliwe <lunga001@gmail.com>
2024-01-14 17:19:27 +0200

    Batch Priority

--- src/bika/aquaculture/browser/listingview/batchfolder.py
+++ src/bika/aquaculture/browser/listingview/batchfolder.py
@@ -21,9 +21,28 @@ class BatchFolderContentsListingViewAdapter(object):
         if not is_installed():
             return
         self.listing.title = self.context.translate(_("Cases"))
+        batch_priority = [
+            ("Priority",
+             {"toggle": False, "sortable": True, "title": ""},
+             )
+        ]
+        self.listing.columns.update(batch_priority)
+        for i in range(len(self.listing.review_states)):
+            self.listing.review_states[i]["columns"].insert(0, "Priority")
 
     def folder_item(self, obj, item, index):
         if not is_installed():
             return item
+        batch = obj.getObject()
+        batch_priority = batch.BatchPriority
+        priority = "3"
+        priority_text = "Routine"
+        if batch_priority == "rush":
+            priority = "1"
+            priority_text = "Rush"
+        priority_div = """<div class="priority-ico priority-%s">
+                          <span class="notext">%s</span><div>
+                       """
+        item["replace"]["Priority"] = priority_div % (priority, priority_text)
 
         return item
